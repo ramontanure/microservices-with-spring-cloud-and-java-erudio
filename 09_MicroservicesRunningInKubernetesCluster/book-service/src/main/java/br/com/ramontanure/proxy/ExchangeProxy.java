b47594c7096f8f741b29eeb4f107e9982d83630d
Ramon Tanure Carvalho <ramon_tanure_carvalho@hotmail.com>
2026-04-21 20:43:54 -0300

    Teste deployment

--- 09_MicroservicesRunningInKubernetesCluster/book-service/src/main/java/br/com/ramontanure/proxy/ExchangeProxy.java
+++ 09_MicroservicesRunningInKubernetesCluster/book-service/src/main/java/br/com/ramontanure/proxy/ExchangeProxy.java
@@ -2,27 +2,21 @@ package br.com.ramontanure.proxy;
 
 import br.com.ramontanure.dto.Exchange;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-//@FeignClient(name = "exchange-service")
-@FeignClient(name = "exchange-service", url = "{EXCHANGE_SERVICE_SERVICE_HOST:host.docker.internal}:8000")
-// 🔗 Feign Client:
-// Permite chamar outro microserviço como se fosse método Java
-// Usa Eureka para descobrir o serviço automaticamente
-
+// @FeignClient(name = "exchange-service")
+//@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_SERVICE_HOST:http://host.docker.internal}:8000")
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_URI:http://host.docker.internal}:8000")
 public interface ExchangeProxy {
 
     @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
-    // 📡 Define o endpoint do outro serviço
-
     public Exchange getExchange(
-            @PathVariable Double amount,
-            @PathVariable String from,
-            @PathVariable String to);
+            @PathVariable("amount") Double amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to);
 }
 
 //EXCHANGE_SERVICE_SERVICE_HOST
